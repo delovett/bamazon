93f7bf7bd761bdd4b6dbcf7e5f9618fec2699e18
delovett <delovett@asu.edu>
2018-10-25 00:10:36 -0700

    Added schema and seeds for Database

---
+++ seeds.sql
@@ -0,0 +1,26 @@
+USE bamazon;
+
+
+INSERT INTO department (department_name, over_head_costs) VALUES ('Electronics', 10000);
+
+INSERT INTO department (department_name, over_head_costs) VALUES ('Clothing', 60000);
+
+INSERT INTO product (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Gameboy', 1, 50, 2000, 0);
+
+INSERT INTO product (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Gameboy DS', 1, 75, 2000, 0);
+
+INSERT INTO product (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Your own imagination', 1, 50, 2000, 0);
+
+INSERT INTO product (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Atari', 1, 75, 2000, 0);
+
+INSERT INTO product (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Switch', 1, 50, 2000, 0);
+
+INSERT INTO product (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Hats', 2, 75, 2000, 0);
+
+INSERT INTO product (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Shoes', 2, 50, 2000, 0);
+
+INSERT INTO product (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Belts', 2, 75, 2000, 0);
+
+INSERT INTO product (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Socks', 2, 50, 2000, 0);
+
+INSERT INTO product (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Gloves', 2, 75, 2000, 0);
