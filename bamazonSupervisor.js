15cb03248cf267c08dd071c78a7d1524b74a8459
delovett <delovett@asu.edu>
2018-10-25 00:19:12 -0700

    Added MySQL connections to all shells

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,6 +1,26 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function(err, res){
+    if(err){
+        throw err;
+    }
+    else{
+        //console.log("Database connection made!")
+    }
+})
+
+
+
 inquirer
     .prompt([
         {
